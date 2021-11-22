d17cd1c0a15d60b65b9b842e1669e537beda1f4b
VladislavApelgants <79588148+VladislavApelgants@users.noreply.github.com>
2021-11-22 19:20:31 +0200

    Updates

---
+++ static/js/main.ba842e84.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),i=c.n(s),o=(c(8),c(9),c(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("header",{className:"App-header",children:Object(o.jsx)("p",{children:"lololo"})})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ba842e84.chunk.js.map
